ff05f653a510cd1fb21ef457e4ed979f26331064
sirbiggies <neo.varnhed@gmail.com>
2025-05-22 13:24:21 +0200

    Update about.js

--- about.js
+++ about.js
@@ -14,3 +14,45 @@ const tabCloak = document.createElement("script");
 tabCloak.setAttribute("src", "/js/tab_cloak.js");
 document.head.append(tabCloak);
 script("Injected script 2/5");
+
+function createScriptTag(options) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script')
+
+    if (options.src) {
+      script.src = options.src
+      script.onload = resolve
+      script.onerror = reject
+    } else {
+      resolve()
+    }
+
+    if (options.inline) {
+      script.textContent = options.inline
+    }
+
+    if (options.async) {
+      script.async = true
+    }
+
+    if (options.defer) {
+      script.defer = true
+    }
+
+    if (options.crossOrigin) {
+      script.crossOrigin = options.crossOrigin
+    }
+
+    if (options.attributes) {
+      for (const [key, value] of Object.entries(options.attributes)) {
+        script.setAttribute(key, value)
+      }
+    }
+
+    document.head.appendChild(script)
+
+    if (!options.src) {
+      resolve()
+    }
+  })
+}
