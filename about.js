e9dfa2df6569884e476d09dad02ddc0a361227e0
sirbiggies <neo.varnhed@gmail.com>
2025-05-22 13:11:41 +0200

    Update about.js

--- about.js
+++ about.js
@@ -1,13 +1,3 @@
-
-console.warn(
-  "%cNote!",
-  "color: purple; font-weight: 600; background: yellow; padding: 0 5px; border-radius: 5px",
-  "If you want to add my games to your site, please reach out at my email: echo-the-coder@tuta.io\nPlease do not just add them without asking me first! Thank you!"
-);
-
-function script(text) {
-  console.log("%cScript Injection", "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px", text);
-}
 var gaenabled = window.localStorage.getItem("ga");
 if (gaenabled == "false") {
   script("Skipped GA injection because it is disabled by the user.");
@@ -28,9 +18,3 @@ const tabCloak = document.createElement("script");
 tabCloak.setAttribute("src", "/js/tab_cloak.js");
 document.head.append(tabCloak);
 script("Injected script 2/5");
-
-const arc = document.createElement("script");
-arc.setAttribute("async", "");
-arc.setAttribute("src", "https://arc.io/widget.min.js#eRPHFgiC");
-document.head.append(arc);
-script("Injected script 3/5");
